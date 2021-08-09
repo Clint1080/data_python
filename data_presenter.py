4665517ba0b0fe8547b84b30b82c9173a8cbca97
Clint Edwards <cedwards1080@gmail.com>
2021-08-09 15:34:39 -0600

    Finished part 2

---
+++ data_presenter.py
@@ -0,0 +1,21 @@
+open_file = open("CupcakeInvoices.csv")
+
+purchase_amounts = []
+
+for row in open_file:
+    # print(row) # checking that it works
+    row = row.rstrip('/').split(',')
+    
+    # print(row[2]) # Cupcake type
+    
+    purchase_total = float(row[3]) * float(row[4]) # row[3] is amount purchased and row[4] is price per each
+    # print(purchase_total) # Checking that I was able to get the totals
+    
+    purchase_amounts.append(purchase_total) # Putting purchase amounts into a new array
+    #print(purchase_amounts)
+    
+    
+purchase_total = sum(purchase_amounts) # adding all of the individual purches together
+print('All the cupcake purchases = $', purchase_total)
+
+open_file.close()
